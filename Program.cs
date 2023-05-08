d5ab9388698436855a006f1ba6987dbbbb885a32
Nikhitha <nikitha.kunapareddy@sailssoftware.com>
2023-05-08 15:06:47 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+namespace AsynchronousProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main Method Started......");
+
+            SomeMethod();
+
+            Console.WriteLine("Main Method End");
+            Console.ReadKey();
+        }
+
+        public static void SomeMethod()
+        {
+            Console.WriteLine("Some Method Started......");
+
+            Thread.Sleep(TimeSpan.FromSeconds(10));
+            Console.WriteLine("\n");
+            Console.WriteLine("Some Method End");
+        }
+    }
+}
